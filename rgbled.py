694e2b1cca5c3c49529a06b52d443e19aa0a81de
Vakarisnip <aurnip@yahoo.co.uk>
2025-09-07 18:10:07 +0100

    Update rgbled.py

--- rgbled.py
+++ rgbled.py
@@ -1,23 +1,27 @@
-from machine import Pin
-from utime import sleep
-red = Pin(15, Pin.OUT)
-green = Pin(14, Pin.OUT)
-blue = Pin(13, Pin.OUT)
-red.value(0)
-green.value(0)
-blue.value(0)
-print("hello world, from github")
+from time import sleep
+import board
+import digitalio
+red = digitalio.DigitalInOut(board.GP15)
+red.direction = digitalio.Direction.OUTPUT
+green = digitalio.DigitalInOut(board.GP14)
+green.direction = digitalio.Direction.OUTPUT
+blue = digitalio.DigitalInOut(board.GP13)
+blue.direction = digitalio.Direction.OUTPUT
+red.value = False
+green.value = False
+blue.value = False
+print("hello world, from github (CircuitPython)")
 while True:
-  blue.value(0)
-  red.value(1)
-  sleep(0.5)
-  green.value(1)
-  sleep(0.5)
-  red.value(0)
-  sleep(0.5)
-  blue.value(1)
-  sleep(0.5)
-  green.value(0)
-  sleep(0.5)
-  red.value(1)
-  sleep(0.5)
+    blue.value = False
+    red.value = True
+    sleep(0.5)
+    green.value = True
+    sleep(0.5)
+    red.value = False
+    sleep(0.5)
+    blue.value = True
+    sleep(0.5)
+    green.value = False
+    sleep(0.5)
+    red.value = True
+    sleep(0.5)
